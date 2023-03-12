3678c184b9b787360a2b7aeac9d67960d26fb780
abhishekgarfield <abhishek2759@gmail.com>
2023-03-12 11:22:23 +0530

    starting flappy bird

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+
+
+const App = () =>{
+  return (
+      <SafeAreaView>
+        <View>
+          <Text>Hello Abhishek</Text>
+        </View>
+      </SafeAreaView>
+  )
+}
+
+export default App;
